4baee13f970faf044e2cbed9726c91451a9c4a5b
Arley HR <arley.hr@hotmail.com>
2015-09-28 09:38:57 -0500

    Last

--- dev/e2e/chat/chat.po.js
+++
@@ -1,9 +0,0 @@
-/* global element, by */
-'use strict';
-
-function ChatPage() {
-  this.text = element(by.tagName('p'));
-  this.heading = element(by.tagName('h2'));
-}
-
-module.exports = ChatPage;
